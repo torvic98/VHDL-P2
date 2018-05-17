7b0d22a359ce46b0942fa5f239942f3a6002d3d8
Victor M. Batlle <736478@unizar.es>
2018-05-17 18:27:31 +0200

    Initial commit

---
+++ UC_MC.vhd
@@ -0,0 +1,124 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    13:38:18 05/15/2014 
+-- Design Name: 
+-- Module Name:    UC_slave - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: la UC incluye un contador de 2 bits para llevar la cuenta de las transferencias de bloque y una máquina de estados
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity UC_MC is
+    Port ( 	clk : in  STD_LOGIC;
+			reset : in  STD_LOGIC;
+			RE : in  STD_LOGIC; --RE y WE son las ordenes del MIPs
+			WE : in  STD_LOGIC;
+			hit : in  STD_LOGIC; --se activa si hay acierto
+			dirty_bit : in  STD_LOGIC; --avisa si el bloque a reemplazar es sucio
+			bus_TRDY : in  STD_LOGIC; --indica que la memoria no puede realizar la operación solicitada en este ciclo
+			Bus_DevSel: in  STD_LOGIC; --indica que la memoria ha reconocido que la dirección está dentro de su rango
+			MC_RE : out  STD_LOGIC; --RE y WE de la MC
+            MC_WE : out  STD_LOGIC;
+            bus_RE : out  STD_LOGIC; --RE y WE de la MC
+            bus_WE : out  STD_LOGIC;
+            MC_tags_WE : out  STD_LOGIC; -- para escribir la etiqueta en la memoria de etiquetas
+            palabra : out  STD_LOGIC_VECTOR (1 downto 0);--indica la palabra actual dentro de una transferencia de bloque (1ª, 2ª...)
+            mux_origen: out STD_LOGIC; -- Se utiliza para elegir si el origen de la dirección y el dato es el Mips (cuando vale 0) o la UC y el bus (cuando vale 1)
+            ready : out  STD_LOGIC; -- indica si podemos procesar la orden actual del MIPS en este ciclo. En caso contrario habrá que detener el MIPs
+            MC_send_addr : out  STD_LOGIC; --ordena que se envíen la dirección y las señales de control al bus
+            MC_send_data : out  STD_LOGIC; --ordena que se envíen los datos
+            Frame : out  STD_LOGIC; --indica que la operación no ha terminado
+			Send_dirty	: out  STD_LOGIC; --indica que hay que enviar el bloque sucio por el bus
+			Update_dirty	: out  STD_LOGIC; --indica que hay que actualizar el bit dirty
+			Replace_block	: out  STD_LOGIC -- indica que se ha reemplzado un bloque
+           );
+end UC_MC;
+
+architecture Behavioral of UC_MC is
+
+
+component counter_2bits is
+		    Port ( clk : in  STD_LOGIC;
+		           reset : in  STD_LOGIC;
+		           count_enable : in  STD_LOGIC;
+		           count : out  STD_LOGIC_VECTOR (1 downto 0)
+					  );
+end component;		           
+type state_type is (Inicio); -- Poner aquí el nombre de los estados. Usad nombres descriptivos
+signal state, next_state : state_type; 
+signal last_word: STD_LOGIC; --se activa cuando se está pidiendo la última palabra de un bloque
+signal count_enable: STD_LOGIC; -- se activa si se ha recibido una palabra de un bloque para que se incremente el contador de palabras
+signal palabra_UC : STD_LOGIC_VECTOR (1 downto 0);
+begin
+ 
+ 
+--el contador nos dice cuantas palabras hemos recibido. Se usa para saber cuando se termina la transferencia del bloque y para direccionar la palabra en la que se escribe el dato leido del bus en la MC
+word_counter: counter_2bits port map (clk, reset, count_enable, palabra_UC); --indica la palabra actual dentro de una transferencia de bloque (1ª, 2ª...)
+
+last_word <= '1' when palabra_UC="11" else '0';--se activa cuando estamos pidiendo la última palabra
+
+palabra <= palabra_UC;
+
+   SYNC_PROC: process (clk)
+   begin
+      if (clk'event and clk = '1') then
+         if (reset = '1') then
+            state <= Inicio;
+         else
+            state <= next_state;
+         end if;        
+      end if;
+   end process;
+ 
+   --MEALY State-Machine - Outputs based on state and inputs
+   OUTPUT_DECODE: process (state, hit, last_word, bus_TRDY, RE, WE, dirty_bit, Bus_DevSel)
+   begin
+			  -- valores por defecto, si no se asigna otro valor en un estado valdrán lo que se asigna aquí
+		MC_WE <= '0';
+		bus_RE <= '0';
+		bus_WE <= '0';
+        MC_tags_WE <= '0';
+        MC_RE <= RE;--leemos en la cache si se solicita una lectura de una instrucción
+        ready <= '0';
+        mux_origen <= '0';
+        MC_send_addr <= '0';
+        MC_send_data <= '0';
+        next_state <= state;  
+		count_enable <= '0';
+		Frame <= '0';
+		Send_dirty <= '0';
+		Update_dirty <= '0';
+		Replace_block <= '0';	
+			
+        -- Estado Inicio          
+        if (state = Inicio and RE= '0' and WE= '0') then -- si no piden nada no hacemos nada
+			next_state <= Inicio;
+			ready <= '1';
+        -- Incluir aquí vuestra máquina de estados. Mirar el ejemplo en las transparencas de VHDL
+		end if;
+   end process;
+ 
+   
+end Behavioral;
+
---
+++ UD.vhd
@@ -0,0 +1,33 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+--Mux 4 a 1
+entity UD is
+    Port ( 	Reg_Rs_ID: in  STD_LOGIC_VECTOR (4 downto 0); --registros Rs y Rt en la etapa ID
+		   Reg_Rt_ID	: in  STD_LOGIC_VECTOR (4 downto 0);
+			MemRead_EX	: in std_logic; -- información sobre la instrucción en EX (destino, si lee de memoria y si escribe en registro)
+			RegWrite_EX	: in std_logic;
+			RW_EX			: in  STD_LOGIC_VECTOR (4 downto 0);
+			RegWrite_Mem	: in std_logic;-- informacion sobre la instruccion en Mem (destino y si escribe en registro)
+			RW_Mem			: in  STD_LOGIC_VECTOR (4 downto 0);
+			IR_op_code	: in  STD_LOGIC_VECTOR (5 downto 0); -- código de operación de la instrucción en IEEE
+            PCSrc			: in std_logic; -- 1 cuando se produce un salto 0 en caso contrario
+			FP_add_EX	: in std_logic; -- Indica si la instrucción en EX es un ADDFP
+			FP_done		: in std_logic; -- Informa cuando la operación de suma en FP ha terminado
+			Kill_IF		: out  STD_LOGIC; -- Indica que la instrucción en IF no debe ejecutarse (fallo en la predicción de salto tomado)
+			Parar_ID		: out  STD_LOGIC; -- Indica que las etapas ID y previas deben parar
+			Parar_EX		: out  STD_LOGIC); -- Indica que las etapas EX y previas deben parar
+end UD;
+Architecture Behavioral of UD is
+	signal Parar_ID_BEQ, Parar_ID_UA, Parar_EX_ADDFP : std_logic;
+begin
+	-- AHora mismo no hace nada. Hay que diseñar la lógica que genera estas señales.
+	-- Además hay que conectar estas señales con los elementos adecuados para que las órdenes que indican se realicen
+	Parar_ID_BEQ <= '1' when (IR_op_code="000100" and ( (Reg_Rs_ID=RW_EX and RegWrite_EX='1') or (Reg_Rt_ID=RW_EX and RegWrite_EX='1')
+	                       or (Reg_Rs_ID=RW_MEM and RegWrite_MEM='1') or (Reg_Rt_ID=RW_MEM and RegWrite_MEM='1') ) )
+				else '0';
+	Parar_ID_UA <= '1' when (MemRead_EX='1' and (RW_EX=Reg_Rs_ID or RW_EX=Reg_Rt_ID)) else '0';
+	Parar_EX_ADDFP <= '1' when (FP_add_EX='1' and FP_done='0') else '0';
+	Kill_IF <= PCSrc and not (Parar_ID_BEQ or Parar_ID_UA or Parar_EX_ADDFP);
+	Parar_ID <= Parar_ID_BEQ or Parar_ID_UA;
+	Parar_EX <= Parar_EX_ADDFP;
+end Behavioral;
